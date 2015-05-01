232e877b1dbbf3d10a06178d55529b706fa4c620
Odhrean <dirk.lietz@posteo.de>
2015-05-01 14:16:10 +0200

    Initial commit

---
+++ WindowScrape/WindowScrape/Constants/PositioningFlags.cs
@@ -0,0 +1,38 @@
+﻿namespace WindowScrape.Constants
+{
+    using System;
+    using System.Runtime.InteropServices;
+    [Flags]
+    internal enum PositioningFlags
+    {
+        SWP_ASYNCWINDOWPOS = 0x4000,
+        SWP_DEFERERASE = 0x2000,
+        SWP_DRAWFRAME = 0x20,
+        SWP_FRAMECHANGED = 0x20,
+        SWP_HIDEWINDOW = 0x80,
+        SWP_NOACTIVATE = 0x10,
+        SWP_NOCOPYBITS = 0x100,
+        SWP_NOMOVE = 2,
+        SWP_NOOWNERZORDER = 0x200,
+        SWP_NOREDRAW = 8,
+        SWP_NOREPOSITION = 0x200,
+        SWP_NOSENDCHANGING = 0x400,
+        SWP_NOSIZE = 1,
+        SWP_NOZORDER = 4,
+        SWP_SHOWWINDOW = 0x40
+    }
+
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WINDOWPLACEMENT
+    {
+        public int length;
+        public int flags;
+        public int showCmd;
+        public System.Drawing.Point ptMinPosition;
+        public System.Drawing.Point ptMaxPosition;
+        public System.Drawing.Rectangle rcNormalPosition;
+    }
+
+}
+
---
+++ WindowScrape/WindowScrape/Types/HwndObject.cs
@@ -0,0 +1,237 @@
+﻿namespace WindowScrape.Types
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Runtime.CompilerServices;
+    using WindowScrape.Constants;
+    using WindowScrape.Static;
+
+    public class HwndObject
+    {
+
+        public HwndObject(IntPtr hwnd)
+        {
+            this.Hwnd = hwnd;
+        }
+
+        public void Activate()
+        {
+            HwndInterface.ActivateWindow(this.Hwnd);
+        }
+
+        public int GetWindowStyle()
+        {
+            WINDOWPLACEMENT placement = HwndInterface.GetPlacement(this.Hwnd);
+            return placement.showCmd;
+        }
+
+        public void DisplayOnScreen(System.Windows.Forms.Screen target)
+        {
+            WINDOWPLACEMENT placement = HwndInterface.GetPlacement(this.Hwnd);
+            placement.ptMaxPosition = new System.Drawing.Point(target.WorkingArea.X, target.WorkingArea.Y);
+            placement.ptMinPosition = new System.Drawing.Point(target.WorkingArea.X, target.WorkingArea.Y);
+            placement.rcNormalPosition = new System.Drawing.Rectangle(target.WorkingArea.X, target.WorkingArea.Y, target.WorkingArea.X + target.WorkingArea.Width,target.WorkingArea.Y + target.WorkingArea.Height);
+            placement.showCmd = 2; // Minimized
+            placement.flags = 0x0002;
+            HwndInterface.SetWindowPlacement(this.Hwnd, ref placement);
+            Maximize();
+        }
+
+        public WINDOWPLACEMENT GetWindowPlacement()
+        {
+            return HwndInterface.GetPlacement(this.Hwnd);
+        }
+
+        public void SetWindowPlacement(WINDOWPLACEMENT placement)
+        {
+            HwndInterface.SetWindowPlacement(this.Hwnd, ref placement);
+        }
+        public void Maximize()
+        {
+            HwndInterface.ShowWindow(this.Hwnd, 3);
+        }
+        public void Minimize()
+        {
+            HwndInterface.ShowWindow(this.Hwnd, 6);
+        }
+        public void ShowNormal()
+        {
+            HwndInterface.ShowWindow(this.Hwnd, 1);
+        }
+
+        public void Click()
+        {
+            HwndInterface.ClickHwnd(this.Hwnd);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (object.ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (object.ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (!(obj.GetType() == typeof(HwndObject)))
+            {
+                return false;
+            }
+            return this.Equals((HwndObject) obj);
+        }
+
+        public bool Equals(HwndObject obj)
+        {
+            if (object.ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            return (object.ReferenceEquals(this, obj) || obj.Hwnd.Equals(this.Hwnd));
+        }
+
+        public HwndObject GetChild(string cls, string title)
+        {
+            return new HwndObject(HwndInterface.GetHwndChild(this.Hwnd, cls, title));
+        }
+
+        public List<HwndObject> GetChildren()
+        {
+            List<HwndObject> list = new List<HwndObject>();
+            foreach (IntPtr ptr in HwndInterface.EnumChildren(this.Hwnd))
+            {
+                list.Add(new HwndObject(ptr));
+            }
+            return list;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Hwnd.GetHashCode();
+        }
+
+        public int GetMessageInt(WM msg)
+        {
+            return HwndInterface.GetMessageInt(this.Hwnd, msg);
+        }
+
+        public string GetMessageString(WM msg, uint param)
+        {
+            return HwndInterface.GetMessageString(this.Hwnd, msg, param);
+        }
+
+        public HwndObject GetParent()
+        {
+            return new HwndObject(HwndInterface.GetHwndParent(this.Hwnd));
+        }
+
+        public static HwndObject GetWindowByTitle(string title)
+        {
+            return new HwndObject(HwndInterface.GetHwndFromTitle(title));
+        }
+
+        public static List<HwndObject> GetWindows()
+        {
+            List<HwndObject> list = new List<HwndObject>();
+            foreach (IntPtr ptr in HwndInterface.EnumHwnds())
+            {
+                list.Add(new HwndObject(ptr));
+            }
+            return list;
+        }
+
+        
+        public static bool operator ==(HwndObject a, HwndObject b)
+        {
+            return (a.Hwnd == b.Hwnd);
+        }
+
+        public static bool operator !=(HwndObject a, HwndObject b)
+        {
+            return !(a == b);
+        }
+        
+        public void SendMessage(WM msg, uint param1, string param2)
+        {
+            HwndInterface.SendMessage(this.Hwnd, msg, param1, param2);
+        }
+
+        public void SendMessage(WM msg, uint param1, uint param2)
+        {
+            HwndInterface.SendMessage(this.Hwnd, msg, param1, param2);
+        }
+
+        public void SetWindowPos(IntPtr hWndInsertAfter, int x, int y, int cx, int cy, UInt32 uFlags)
+        {
+            HwndInterface.SetWindowPos(this.Hwnd, hWndInsertAfter, x, y, cx, cy, uFlags);
+        }
+
+        public override string ToString()
+        {
+            Point location = this.Location;
+            System.Drawing.Size size = this.Size;
+            return string.Format("({0}) {1},{2}:{3}x{4} \"{5}\"", new object[] { this.Hwnd, location.X, location.Y, size.Width, size.Height, this.Title });
+        }
+
+        public string ClassName
+        {
+            get
+            {
+                return HwndInterface.GetHwndClassName(this.Hwnd);
+            }
+        }
+
+        public IntPtr Hwnd { get; private set; }
+
+        public Point Location
+        {
+            get
+            {
+                return HwndInterface.GetHwndPos(this.Hwnd);
+            }
+            set
+            {
+                HwndInterface.SetHwndPos(this.Hwnd, value.X, value.Y);
+            }
+        }
+
+        public System.Drawing.Size Size
+        {
+            get
+            {
+                return HwndInterface.GetHwndSize(this.Hwnd);
+            }
+            set
+            {
+                HwndInterface.SetHwndSize(this.Hwnd, value.Width, value.Height);
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                return HwndInterface.GetHwndText(this.Hwnd);
+            }
+            set
+            {
+                HwndInterface.SetHwndText(this.Hwnd, value);
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return HwndInterface.GetHwndTitle(this.Hwnd);
+            }
+            set
+            {
+                HwndInterface.SetHwndTitle(this.Hwnd, value);
+            }
+        }
+
+    }
+}
+
